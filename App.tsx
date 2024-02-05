9cf11972c32dd79042d98ad20ef079e38874414e
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-02-05 23:19:04 +0500

    env setup complete work from tomorrow

--- App.tsx
+++ App.tsx
@@ -1,16 +1,49 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
-    <View>
-      <Text>lorem</Text>
-      <Text>lorem</Text>
-      <Text>lorem</Text>
-      <Text>lorem</Text>
-      <Text>lorem</Text>
+    <View style={styles.container}>
+      <Text>what is this</Text>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  restaurantItem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  restaurantLogo: {
+    width: 100,
+    height: 100,
+  },
+})
