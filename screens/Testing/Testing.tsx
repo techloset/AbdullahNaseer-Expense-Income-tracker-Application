f54abc199839ebfb7baa1c1f31543eaa46c84eee
abdullahnaseer01 <mianabdullah125125@gmail.com>
2024-02-16 18:43:06 +0500

    firebase settingup

---
+++ screens/Testing/Testing.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+
+const Testing = () => {
+  return (
+    <View>
+      <TextInput/>
+    </View>
+  )
+}
+
+export default Testing
+
+const styles = StyleSheet.create({})
